e41abb589bfdd57c057a8b90a4a27b5ae7c52822
swedge <scott.wedge@gmail.com>
2020-06-30 23:51:32 -0400

    Setup default grid size and ship sizes and numbers

--- battleship.py
+++ battleship.py
@@ -8,17 +8,42 @@
     20. Decide on number of shots (default to one) or one per unsunk ship or one per unhit grid space
     22. Decide on smart or random shooting.
     30. Place ships (random or deliberately).
-    40. Start game.
-    50. Start displaying statistics.
-    60. Update display with every shot.
-    70. Add ability to: save game mid-session, 
-    80.
-    90.
-    100.
+    40. Display setup.
+    100. Start game.
+    200. Start displaying statistics.
+    300. Update display with every shot.
+    1000. Add ability to: save game mid-session, 
 """
 
-"""Import statements"""
+# Import statements
 import os
 
-def setup():
+
+# Set default values
+default_x = 10
+default_y = 10
+biggest_ship = 5
+big_ship = 4
+medium_ship_1 = 3
+medium_ship_2 = 3
+smallest_ship = 2
+
+
+def setup_grid():
+    """ Use default values to setup "Classic" game size"""
+    max_x = default_x
+    max_y = default_y
     pass
+
+def setup_ships():
+    """ Use default ship size and numbers to setup "Classic" game size"""
+    ships = []
+    pass
+
+
+def main():
+    setup_grid()
+    setup_ships()
+
+
+main()
