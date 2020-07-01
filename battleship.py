f8cf6e8f56997e7f51fe5872f009f98c60fc3aa1
swedge <scott.wedge@gmail.com>
2020-06-30 23:38:03 -0400

    Initial commit with stages of program development

---
+++ battleship.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" Sections of the program are:
+    10. Decide on size of grid and number and sizes of ships.
+    12. Default to 10x10 size 
+    14. Default number of ships/sizes as: one each of Carrier (5), Battleship (4), 
+        Cruiser(3), Submarine(3), Destroyer(2).
+    20. Decide on number of shots (default to one) or one per unsunk ship or one per unhit grid space
+    22. Decide on smart or random shooting.
+    30. Place ships (random or deliberately).
+    40. Start game.
+    50. Start displaying statistics.
+    60. Update display with every shot.
+    70. Add ability to: save game mid-session, 
+    80.
+    90.
+    100.
+"""
+
+"""Import statements"""
+import os
+
+def setup():
+    pass
