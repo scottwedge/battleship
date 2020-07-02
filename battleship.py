eb7c5ba33d286c1a3175fe2577658e2ee845e16c
swedge <scott.wedge@gmail.com>
2020-07-02 15:46:59 -0400

    Create random number to position ship

--- battleship.py
+++ battleship.py
@@ -2,35 +2,36 @@
 
 """ Sections of the program are:
     10. Decide on size of grid and number and sizes of ships.
-    12. Initially default to 10x10 size 
-    14. Initially default number of ships/sizes as: one each of Carrier (5), Battleship (4), 
+    12. Initially default to 10x10 size
+    14. Initially default number of ships/sizes as: one each of Carrier (5), Battleship (4),
         Cruiser(3), Submarine(3), Destroyer(2).
-    20. Decide on number of shots (default to one) or one per unsunk ship or one per unhit grid space
+    20. Decide on number of shots (default to one) or one per unsunk ship or one per unhit grid
     22. Decide on smart or random shooting.
     30. Place ships (random or deliberately).
     40. Display setup.
     100. Start game.
     200. Start displaying statistics.
     300. Update display with every shot.
-    1000. Add ability to: save game mid-session, 
+    1000. Add ability to: save game mid-session,
 """
 
 # Import statements
-import os
+import random
+from datetime import datetime
+
+
+# Initialize random number generator
+random.seed(datetime.now())
 
 
 # Set default values
 default_x = 10  # X axis (A through J)
-default_y = 10  # Y axis (1 through 10) (top left grid point is A-1, bottom right grid point is J-10)
-biggest_ship = 5
-big_ship = 4
-medium_ship_1 = 3
-medium_ship_2 = 3
-smallest_ship = 2
+default_y = 15  # Y axis (1 through 10) (top left grid is A-1, bottom right grid is J-10)
+
 
 def print_column_heading():
     col = "ABCDEFGHIJ"
-    heading =[0]
+    heading = [0]
     for j in col:
         heading.append(j)
     print(heading)
@@ -40,7 +41,9 @@ def setup_grid():
     """ Use default values to setup "Classic" game size of 10x10 grid"""
     max_x = default_x
     max_y = default_y
+    return (max_x, max_y)
 
+def print_empty_grid(max_x, max_y):
     print_column_heading()
     y = 1
     while y <= max_y:
@@ -51,17 +54,38 @@ def setup_grid():
             x = x + 1
         print(row)    
         y = y + 1
-    pass
 
 def setup_ships():
-    """ Use default ship size and numbers to setup "Classic" game size"""
-    ships = []
-    pass
+    """ Use default ship size and numbers to setup "Classic" game size
+        Create tuple with unique ship type, character to place on grid, number of grid spots
+    """
+    ships = [("aircraft carrier", "A", 5), ("battleship","B", 4), ("cruiser", "C", 3), \
+            ("submarine", "S", 3), ("destroyer", "D", 2)]
+    for ship in ships:
+        (a, b, c) = ship
+        print(a, b, c)
+
+
+def generate_random(max_x, max_y):
+    num = random.randint(1, max_x * max_y)
+    return num
+
+
+def place_ships(max_x, max_y):
+    """Place ships in order of largest to smallest - seems easiest.
+       Generate random number which determines location"""
+    num = generate_random(max_x, max_y)
+    x = num % max_x
+    y = num // max_x
+    print(num, "translates to x=", x, "and y=", y)
 
 
 def main():
-    setup_grid()
+    (x, y) = setup_grid()
+    print_empty_grid(x, y)
     setup_ships()
+    place_ships(x, y)
 
 
-main()
+if __name__ == "__main__":
+    main()
